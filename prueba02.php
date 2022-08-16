e197aa7e5ba3f334a85583a52292a29ee6e2c15d
MarcoEsquivel1 <marco.esquivel@catolica.edu.sv>
2022-08-16 17:46:32 -0600

    practica 16 de agosto Marco

---
+++ prueba02.php
@@ -0,0 +1,16 @@
+<?php include "includes/header.php"; ?>
+    <?php
+    echo "hola mundo" , "2";
+    echo"</br>";
+    echo ("hola mundo" . "2");
+    echo"</br>";
+    print_r("hola mundo");
+    echo"</br>";
+    var_dump("hola mundo");
+    echo"</br>";
+    $nombre=1;
+    echo "Hola <b> ($nombre + 1) </b>, bienvenido a clase";
+    echo"</br>";
+    echo "hola".($nombre+1)."gusto de verte"
+    ?>
+<?php include "includes/footer.php" ?>
